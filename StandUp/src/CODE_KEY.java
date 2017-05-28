825879822f36b7bedaed6368f55ff5178b14b6ca
NetProject1 <gunheel123@gmail.com>
2017-05-29 02:18:29 +0900

    Add files via upload

---
+++ StandUp/src/CODE_KEY.java
@@ -0,0 +1,20 @@
+package Model;
+
+public class CODE_KEY {
+	public static final String CODE_GAMESTART = "GAMESTART";
+	public static final String CODE_TOKEN_ON = "CODETOKENON";
+	public static final String CODE_TOKEN_OFF = "CODETOKENOFF";
+	public static final String CODE_CALL = "CODECALL";
+	public static final String CODE_DOUBLE = "CODEDOUBLE";
+	public static final String CODE_HALF = "CODEHALF";
+	public static final String CODE_DIE = "CODEDIE";
+	public static final String CODE_GAME = "CODEGAME";
+	public static final String CODE_WIN = "GAMEWIN";
+	public static final String CODE_LOSE = "GAMELOSE";
+	public static final String CODE_GAMEEND = "GAMEEND";
+	
+	//
+	//예시
+	// CODE_GAME_CALL
+	
+}
